6872d774dc395c6df4445671828e5552d1c9ab26
Anastasiia <lukianets.anastasiia@gmail.com>
2025-01-10 21:58:31 +0100

    outputting content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration > 0;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -27,7 +29,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration grater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
